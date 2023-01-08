9305b5fd7b89dbc30f3ee50b7a11f8a88498744d
Vivekumar01 <vivekumar2003bsr@gmail.com>
2023-01-08 14:07:13 +0530

    Contact Complete

--- src/Pages/Portfolio/Portfolio.jsx
+++ src/Pages/Portfolio/Portfolio.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { Helmet } from "react-helmet-async";
 // import Typewriter from "typewriter-effect";
 import { motion } from "framer-motion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleExclamation } from "@fortawesome/free-solid-svg-icons";
 
 const Portfolio = () => {
   const boxVariant = {
@@ -18,7 +20,21 @@ const Portfolio = () => {
         initial="hidden"
         animate="visible"
         className="flex md:flex-row w-full md:w-auto md:mx-12 flex-col-reverse gap-5 md:gap-0 items-center md:my-12 align-middle "
-      >Portfolio</motion.div>
+      >
+        Portfolio
+        <div>
+          <h1 className="flex mx-20 text-red-500 text-lg  items-center">
+            <FontAwesomeIcon
+              className="w-5 h-5 mr-2  sm:mr-6"
+              icon={faCircleExclamation}
+            />
+            The function of this page is not in use!!!
+          </h1>
+          <h1 className="flex mx-20 text-red-500 text-lg  items-center">
+            Try Again Later
+          </h1>
+        </div>
+      </motion.div>
     </motion.div>
   );
 };
