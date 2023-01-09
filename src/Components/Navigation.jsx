f75db10dcca0d407d9247e9cc0bff4d8c54bc622
Vivekumar01 <vivekumar2003bsr@gmail.com>
2023-01-09 15:11:36 +0530

    portfolio

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -6,14 +6,14 @@ function Navigation() {
   return (
     <nav className="bg-transparent  flex w-full justify-between items-center pt-2 px-5 ">
       <div>
-        <span className="text-[25px]">
+        <span className="md:text-[25px] text-[20px]">
           <Link to="/">Vykriea </Link>
         </span>
       </div>
-      <div className="flex items-center gap-5 justify-between">
+      <div className="flex items-center gap-5 text-[14px] md:text-base justify-between">
         <Link to={"/portfolio"}>Portfolio</Link>
         <Link to={"/About Me"} className="">About Me</Link>
-        <Link to={"/Contact Me"}>Contact</Link>
+        <Link to={"/Contact"}>Contact</Link>
 
         <ThemeChanger />
       </div>
